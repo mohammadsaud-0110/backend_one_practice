4e020b833c01fa4531057848c43f44762f2954ef
Mohammad Saud <mdsaud.mrj@gmail.com>
2023-02-20 02:30:13 +0530

    Cors

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ require('dotenv').config()
 
 
 const app=express()
-app.use(express.json());
 app.use(cors())
+app.use(express.json());
+
 
 app.get("/",(req,res)=>{
     res.send("Home Page Notebook")
